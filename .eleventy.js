4de36aa5ee6ba99ed3fd0b79be5a3b87db9fd042
Dennis Heibült <mail@dennisheibuelt.com>
2020-02-14 12:23:35 +0100

    Add syntax highlighting [St. Walker]

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,13 @@
-module.exports = {
-    dir: {
-        includes: "_includes",
-        layouts: "_layouts"
+const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
+
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPlugin(syntaxHighlight);
+
+    return {
+        dir: {
+            includes: "_includes",
+            layouts: "_layouts"
+        }
     }
+
 };
