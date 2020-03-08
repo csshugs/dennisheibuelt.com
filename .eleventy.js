f351d3b66cec8ef8d47503840196567e9d823b5c
Dennis Heibült <mail@dennisheibuelt.com>
2020-03-08 18:49:33 +0100

    Add daily photo #1 []

--- .eleventy.js
+++ .eleventy.js
@@ -3,6 +3,23 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = function(eleventyConfig) {
     eleventyConfig.addPlugin(syntaxHighlight);
 
+    // Find and copy any `.jpg` files, maintaining directory structure.
+    eleventyConfig.addPassthroughCopy("dailyphoto/**/*.jpg");
+
+    // Generate responsive image block.
+    eleventyConfig.addShortcode("image", function(name, alt) {
+        return `<img alt="${alt}"
+src="${name}-320.jpg"
+srcset="${name}-320.jpg 320w,
+        ${name}-480.jpg 480w,
+        ${name}-760.jpg 760w,
+        ${name}-1140.jpg 1140w,
+        ${name}-1520.jpg 1520w,
+        ${name}-2280.jpg 2280w"
+sizes="(min-width: 720px) 42rem,
+       calc(100vw - 2rem)">`;
+    });
+
     return {
         dir: {
             includes: "_includes",
---
+++ dailyphoto/2020/03/08/000001/index.md
@@ -0,0 +1,10 @@
+---
+layout: default.njk
+tags: photo
+category: dailyphoto
+title: "#1"
+publishDate: 08. March 2020
+date: 2020-03-08
+---
+
+{% image "000001", "Empty plate with the rest of muffin wrapping paper next to an empty cup of coffee." %}
